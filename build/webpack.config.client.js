48b7774cc3494cbb02090e6d7730b5e0b338e9be
yishibakaien <646932161@qq.com>
2018-08-31 03:44:58 +0800

    配置完毕 webpack-dev-server

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -2,8 +2,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  // mode: 'development',
   entry: {
     app: path.join(__dirname, '../src/app.js')
   },
@@ -33,3 +35,21 @@ module.exports = {
     })
   ]
 }
+
+if (isDev) {
+  config.devServer = {
+    host: '0.0.0.0', // 比较安全的本机 ip 兼容性较好，方便与别人联调
+    port: '8888',
+    contentBase: path.join(__dirname, '../dist'),
+    // hot: true,
+    overlay: {
+      errors: true // 编译时出现错误，网页中显示错误蒙层
+    },
+    publicPath: '/public',
+    historyApiFallback: {
+      index: '/public/index.html'
+    }
+  }
+}
+
+module.exports = config
