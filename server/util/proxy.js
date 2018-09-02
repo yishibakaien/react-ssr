a6cf94ec6a082398a118aeee0ba8cbcc78e2640f
yishibakaien <646932161@qq.com>
2018-09-02 19:41:54 +0800

    为 GET 请求添加 accessToken

--- server/util/proxy.js
+++ server/util/proxy.js
@@ -22,14 +22,16 @@ module.exports = function(req, res, next) {
     return
   }
 
-  const query = Object.assign({}, req.query)
+  const query = Object.assign({}, req.query, {
+    accesstoken: (needAccessToken && req.method.toUpperCase() === 'GET') ? user.accessToken : ''
+  })
   if (query.needAccessToken) delete query.needAccessToken
 
   axios(`${baseUrl}${path}`, {
     method: req.method,
     params: query,
     data: queryString.stringify(Object.assign({}, req.body, {
-      accesstoken: user.accessToken
+      accesstoken: (needAccessToken && req.method.toUpperCase() === 'POST') ? user.accessToken : ''
     })),
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
